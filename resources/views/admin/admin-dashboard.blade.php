b6ecdd64916caa5c28205676473a2fdf5ce732e0
Jhoedhen <salerajhoedhen@gmail.com>
2025-01-31 09:35:29 +0800

    change style

--- resources/views/admin/admin-dashboard.blade.php
+++ resources/views/admin/admin-dashboard.blade.php
@@ -35,57 +35,65 @@
                 <p>Systematic Buddy 2.0</p>
             </div>
 
-            <a href="{{ route('bulletin')}}">
-                <div class="bulletin input-group mb-0 mt-5 d-flex align-items-center justify-content-center border rounded">
+            <a href="{{ route('bulletin')}}"
+            class="{{ Request::routeIs('bulletin') ? 'active' : '' }}">
+                <div class="side-bar input-group mb-0 mt-5 d-flex align-items-center justify-content-center border rounded">
                     <i class="fa-solid fa-list-ul me-2 icon" style="font-size: 14px;"></i>
                     <span class="navbar-text" style="font-size: 12px;">Bulletin & To Do</span>
                 </div>
             </a>
    
-            <a href="{{ route('postTemplate') }}">
-                <div class="Post-template input-group mt-1 d-flex align-items-center justify-content-center border rounded">
+            <a href="{{ route('postTemplate') }}"
+            class="{{ Request::routeIs('postTemplate') ? 'active' : '' }}">
+                <div class="side-bar input-group mt-1 d-flex align-items-center justify-content-center border rounded">
                     <i class="fa-solid fa-signs-post me-2 icon" style="font-size: 14px;"></i>
                     <span class="navbar-text" style="font-size: 12px;">Post Templates</span>
                 </div>
             </a>
        
-            <a href="{{route('replyTemplate')}}">
-                <div class="Reply-template input-group mt-1 d-flex align-items-center justify-content-center border rounded">
+            <a href="{{route('replyTemplate')}}"
+            class="{{ Request::routeIs('replyTemplate') ? 'active' : '' }}">
+                <div class="side-bar input-group mt-1 d-flex align-items-center justify-content-center border rounded">
                     <i class="fa-solid fa-reply me-2 icon" style="font-size: 14px;"></i>
                     <span class="navbar-text" style="font-size: 12px;">Reply Templates</span>
                 </div>
             </a>
             <hr>
-            <a href="{{route('priceList')}}">
-                <div class="Price-list input-group mt-3 d-flex align-items-center justify-content-center border rounded">
+            <a href="{{route('priceList')}}"
+            class="{{ Request::routeIs('priceList') ? 'active' : '' }}">
+                <div class="side-bar input-group mt-3 d-flex align-items-center justify-content-center border rounded">
                     <i class="fa-solid fa-money-check-dollar me-2 icon" style="font-size: 14px;"></i>
                     <span class="navbar-text" style="font-size: 12px;">Price List</span>
                 </div>
             </a>
 
-            <a href="{{route('quotation')}}">
-                <div class="Quotation input-group mt-1 d-flex align-items-center justify-content-center border rounded">
+            <a href="{{route('quotation')}}"
+            class="{{ Request::routeIs('quotation') ? 'active' : '' }}">
+                <div class="side-bar input-group mt-1 d-flex align-items-center justify-content-center border rounded">
                     <i class="fa-solid fa-quote-left me-2 icon" style="font-size: 14px;"></i>
                     <span class="navbar-text" style="font-size: 12px;">Quotation</span>
                 </div>
             </a>
 
-            <a href="{{route('prospects')}}">
-                <div class="Prospects input-group mt-1 d-flex align-items-center justify-content-center border rounded">
+            <a href="{{route('prospects')}}"
+            class="{{ Request::routeIs('prospects') ? 'active' : '' }}">
+                <div class="side-bar input-group mt-1 d-flex align-items-center justify-content-center border rounded">
                     <i class="fa-solid fa-magnifying-glass-dollar me-2 icon" style="font-size: 14px;"></i>
                     <span class="navbar-text" style="font-size: 12px;">Prospects</span>
                 </div>
             </a>
             <hr>    
-            <a href="{{route('insight')}}">
-                <div class="Insight input-group mt-2 d-flex align-items-center justify-content-center border rounded">
+            <a href="{{route('insight')}}"
+            class="{{ Request::routeIs('insight') ? 'active' : '' }}">
+                <div class="side-bar input-group mt-2 d-flex align-items-center justify-content-center border rounded">
                     <i class="fa-solid fa-lightbulb me-2 icon" style="font-size: 14px;"></i>
                     <span class="navbar-text" style="font-size: 12px;">Insight</span>
                 </div>
             </a>
 
-            <a href="{{route('guides')}}">
-                <div class="Guides input-group mt-2 d-flex align-items-center justify-content-center border rounded">
+            <a href="{{route('guides')}}"
+            class="{{ Request::routeIs('guides') ? 'active' : '' }}">
+                <div class="side-bar input-group mt-2 d-flex align-items-center justify-content-center border rounded">
                     <i class="fa-solid fa-book me-2 icon" style="font-size: 14px;"></i>
                     <span class="navbar-text" style="font-size: 12px;">Guides</span>
                 </div>
